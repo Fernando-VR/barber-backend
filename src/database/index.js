5ba94188c6e29855b3e5149f6404413b5c9ca084
Fernando-VR <f.vargasrodriguez@ugto.mx>
2022-05-21 16:31:47 -0500

    Add windows config and MongoDB Atlas config

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 const debug = require('debug')('app:server-database');
 
 module.exports.ConnectionDb = (app) => {    
+    // Online
+    // const uri = `mongodb+srv://root:root@cluster0.4bo8b.mongodb.net/?retryWrites=true&w=majority`
+    // mongoose.connect(uri, {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true
+    // })
+    //     .then( () => debug('Connect to MongoDB!'))
+    //     .catch( (error) => debug(error));
+    // Local
     mongoose.connect('mongodb://localhost:27017/barber')
         .then( () => debug('Connect to MongoDB!'))
         .catch( (error) => debug(error));
