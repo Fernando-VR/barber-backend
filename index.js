a82c20366e20d7ca6ac2c792198815021df74e31
Fernando-VR <f.vargasrodriguez@ugto.mx>
2022-05-27 22:25:22 -0500

    Add limit of 50mb in middleware

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const { AppointmentAPI } = require('./src/appointment');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+app.use(express.json( { limit: '50mb' } ));
+app.use(express.urlencoded( { limit: '50mb' } ));
 app.use(cors());
 
 IndexAPI(app);
